4d525f19ceacdc31875bddc620636d5e87021f8f
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-23 15:27:09 +0000

    Create Problem_1.sql

---
+++ Problem_1.sql
@@ -0,0 +1,8 @@
+-- # Problem Name: Recyclable and Low Fat Product
+-- ## LeetCode Link: https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+----
+SELECT product_id 
+FROM Products
+WHERE LOW_FATS = 'Y' AND RECYCLABLE = 'Y';
+
