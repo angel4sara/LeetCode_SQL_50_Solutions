ede6858c1c658246f7dcc573d843503bcf4ee491
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-24 12:16:25 +0000

    Create Problem_2.sql

---
+++ Problem_2.sql
@@ -0,0 +1,7 @@
+-- # Problem Name: Find Customer Referee
+-- ## LeetCode Link: https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+-- 
+----
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2;
