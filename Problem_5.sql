b82aa65fb2171bbb3e82847978c251e2eb4a27a2
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-28 22:21:00 +0000

    Update Problem_5.sql

--- Problem_5.sql
+++ Problem_5.sql
@@ -1,6 +1,6 @@
 -- # Problem Name: Invalid Tweets
 -- ## LeetCode Link: https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
--- 
+--
 ----
 SELECT tweet_id 
 FROM Tweets
