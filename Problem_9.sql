6726419fe7283841da91c674a514afa64c03fc29
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-01-28 22:20:35 +0000

    Update Problem_9.sql

--- Problem_9.sql
+++ Problem_9.sql
@@ -1,6 +1,6 @@
 -- # Problem Name: Rising Temperature
 -- ## LeetCode Link: https://leetcode.com/problems/rising-temperature/?envType=study-plan-v2&envId=top-sql-50
--- 
+--
 ----
 SELECT w1.id
 FROM Weather w1
