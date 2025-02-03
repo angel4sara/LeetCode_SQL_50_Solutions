8db349d94d42b08c5334a0b6ff5bf43146fe96ef
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-03 21:29:33 +0000

    Update Problem_1.sql

--- Problem_1.sql
+++ Problem_1.sql
@@ -1,6 +1,5 @@
 -- # Problem Name: Recyclable and Low Fat Product
 -- ## LeetCode Link: https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
--- 
 ----
 SELECT product_id 
 FROM Products
