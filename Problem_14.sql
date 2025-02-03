a7a0a83256bbecf602b264e988614c7073dfbd94
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-03 21:30:15 +0000

    Update Problem_14.sql

--- Problem_14.sql
+++ Problem_14.sql
@@ -1,6 +1,5 @@
 -- # Problem Name: Confirmation Rate
 -- ## LeetCode Link: https://leetcode.com/problems/confirmation-rate/description/?envType=study-plan-v2&envId=top-sql-50
---
 ----
 SELECT s.user_id, 
        ROUND(IFNULL(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.user_id), 0), 2) AS confirmation_rate
