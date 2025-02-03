bdeffe9eb7272d06e6498d222303491da40ad281
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-03 21:29:45 +0000

    Update Problem_10.sql

--- Problem_10.sql
+++ Problem_10.sql
@@ -1,6 +1,5 @@
 -- # Problem Name: Average Time of Process per Machine
 -- ## LeetCode Link: https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
---
 ----
 SELECT a1.machine_id, round(avg(a2.timestamp - a1.timestamp),3) AS processing_time
 FROM Activity a1
