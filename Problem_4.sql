f6d38d709804cb8d5267326bcbb36521c31d8650
angel4sara <158163802+angel4sara@users.noreply.github.com>
2025-02-18 20:12:39 +0000

    Update Problem_4.sql

--- Problem_4.sql
+++ Problem_4.sql
@@ -1,6 +1,5 @@
 -- # Problem Name: Article Views I
 -- ## LeetCode Link: https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
--- 
 ----
 SELECT DISTINCT author_id AS id
 FROM Views
