586e3ffc32235d0e80210e791bcdcb1665230f7c
angel4sara <158163802+angel4sara@users.noreply.github.com>
2026-01-05 11:50:58 +0000

    Create Problem_24.sql

---
+++ Problem_24.sql
@@ -0,0 +1,10 @@
+-- # Problem Name: User Activity for the Past 30 Days I
+-- ## LeetCode Link: https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+----
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date
+ORDER BY day;
