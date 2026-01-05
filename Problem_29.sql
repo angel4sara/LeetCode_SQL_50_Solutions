d4d547b2ab75039da1d1546d47e92ef4dfc25975
angel4sara <158163802+angel4sara@users.noreply.github.com>
2026-01-05 12:42:41 +0000

    Create Problem_29.sql

---
+++ Problem_29.sql
@@ -0,0 +1,8 @@
+-- # Problem Name: Customers Who Bought All Products
+-- ## LeetCode Link: https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
+----
+SELECT
+  customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
