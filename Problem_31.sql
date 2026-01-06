5fa06e9f31dcf34bba1b4468e031cf57438a6c85
angel4sara <158163802+angel4sara@users.noreply.github.com>
2026-01-06 12:58:49 +0000

    Create Problem_31.sql

---
+++ Problem_31.sql
@@ -0,0 +1,12 @@
+-- # Problem Name: Primary Department for Each Employee
+-- ## LeetCode Link: https://leetcode.com/problems/primary-department-for-each-employee/description/?envType=study-plan-v2&envId=top-sql-50
+----
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+    OR employee_id IN (
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(*) = 1
+   );;
