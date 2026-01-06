c688da8f9791496e9c0cbf33e561cb05105569b7
angel4sara <158163802+angel4sara@users.noreply.github.com>
2026-01-06 13:09:46 +0000

    Create Problem_32.sql

---
+++ Problem_32.sql
@@ -0,0 +1,6 @@
+-- # Problem Name: Triangle Judgement
+-- ## LeetCode Link: https://leetcode.com/problems/triangle-judgement/?envType=study-plan-v2&envId=top-sql-50
+----
+SELECT x,y,z,
+CASE WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes' ELSE 'No' END AS triangle
+FROM Triangle;
